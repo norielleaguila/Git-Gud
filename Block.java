3c1d401650a554dc181a9d48df725672de5ebec1
Norielle Aguila <norielle_aguila@dlsu.edu.ph>
2017-07-07 02:56:36 +0800

    inital commit

---
+++ Block.java
@@ -0,0 +1,58 @@
+package tictactoe;
+
+public class Block {
+	private String marker;
+	private boolean flipBlock;
+	private int row, col;
+	
+	public Block(){
+		marker = " ";
+		flipBlock = false;
+	}
+	
+	public Block(int r, int c){
+		marker = " ";
+		flipBlock = false;
+		row = r;
+		col = c;
+	}
+	
+	public Block(boolean f, int r, int c){
+		marker = " ";
+		flipBlock = f;
+		row = r;
+		col = c;
+	}
+
+	public String getMarker() {
+		return marker;
+	}
+
+	public void setMarker(String marker) {
+		this.marker = marker;
+	}
+
+	public boolean isFlipBlock() {
+		return flipBlock;
+	}
+
+	public void setFlipBlock(boolean flipBlock) {
+		this.flipBlock = flipBlock;
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+	public int getCol() {
+		return col;
+	}
+
+	public void setCol(int col) {
+		this.col = col;
+	}
+}
---
+++ Board.java
@@ -0,0 +1,19 @@
+package tictactoe;
+
+public class Board {
+
+	private Block[][] blocks;
+	
+	public Board(){
+		
+	}
+	
+	public void makeBlock(){
+		
+	}
+	
+	public void chooseFlip(boolean middle){
+		
+	}
+	
+}
